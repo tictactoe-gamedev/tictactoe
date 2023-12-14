9459aca959e757006793e46e6d639c7efed5b6b5
kemal <kemal@simulakra.de>
2023-12-15 01:45:12 +0300

    initial commit

---
+++ NewProject/Assets/Scripts/ExampleA.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class ExampleA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int age = 40; 
+        
+        Debug.Log("My age is : " + age);
+
+        AnotherClass a = new AnotherClass();
+
+        a.AnotherAge = 20;
+        
+        a.Something();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
