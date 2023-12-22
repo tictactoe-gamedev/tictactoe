3f10ecbba359fe28e5d6281665543654f3f1a3d9
kemal <kemal@simulakra.de>
2023-12-23 01:29:07 +0300

    nuked old project

--- unreal_project/Source/unreal_project/unreal_project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "unreal_project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal_project, "unreal_project" );
