98cc89cf298bf07a91f9cac9a337fcb3056ded6c
mmatic90 <maticmaurizio@gmail.com>
2023-12-27 20:37:39 +0100

    Quest 2.2 update for Voxel God Quest

--- Assets/Scripts/FinecraftGod.cs
+++ Assets/Scripts/FinecraftGod.cs
@@ -1,57 +1,214 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FinecraftGod : MonoBehaviour
 {
     public GameObject voxelPrefab;
 
-    public Vector3 universeSize;
+    private Vector3 universeSize;
 
-    public int maxVoxelCount = 100;
+    public Material[] voxelMaterials;
+
+    private int maxVoxelCount;
+
+    private float noSpawnProbability = 0.2f;
+
+    public float[] baseSpawnPossibilities;
+    public float[] goldSpawnModifiers;
+    public float[] silverSpawnModifiers;
+    public float[] bronzeSpawnModifiers;
+    public float[] platinumSpawnModifiers;
+
+    private Vector3[] neighborOffsets = new Vector3[] {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back};
 
     private int totalVoxelCount => Voxel.goldVoxelCount + Voxel.silverVoxelCount + Voxel.bronzeVoxelCount + Voxel.platinumVoxelCount;
 
+    private float voxelCreationCooldown = 0.01f;
+    private float timeSinceLastVoxelCreation;
+
+    private HashSet<Vector3> createdVoxelPositions = new HashSet<Vector3>();
+
     void Start()
     {
-        universeSize = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+        universeSize = new Vector3(Random.Range(1, 11), Random.Range(1, 11), Random.Range(1, 11));
         Debug.Log($"World Created: X: {universeSize.x}, Y: {universeSize.y}, Z:{universeSize.z}");
+        maxVoxelCount = (int)universeSize.x * (int)universeSize.y * (int)universeSize.z;
+        Debug.Log($"Max Voxel Count: {maxVoxelCount}");
+        GeneratePlane();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            GenerateRandomVoxel();
-        }
+        timeSinceLastVoxelCreation += Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.K) && timeSinceLastVoxelCreation >= voxelCreationCooldown)
+            //for(int i=0; i<maxVoxelCount; i++)
+            {
+                if(timeSinceLastVoxelCreation >= voxelCreationCooldown)
+                {
+                    if (totalVoxelCount == maxVoxelCount)
+                    {
+                        Debug.Log("Maximum voxel count reached. Cannot generate more voxels.");
+                        return;
+                    }
+                    else
+                    {
+                        GenerateRandomVoxel();
+                        timeSinceLastVoxelCreation = 0.0f;
+                    }
+                }
+ 
+            }
     }
 
     void GenerateRandomVoxel()
     {
+        float noSpawnRandom = Random.Range(0f, 1f);
 
-        if (totalVoxelCount == maxVoxelCount)
+        if (noSpawnRandom <= noSpawnProbability)
         {
-            Debug.Log("Maximum voxel count reached. Cannot generate more voxels.");
+            Debug.Log("Voxel not spawned.");
             return;
         }
 
-        int typeRandom = Random.Range(0, 4);
+        float xPos = Mathf.RoundToInt(Random.Range(0, universeSize.x-1));
+        float yPos = Mathf.RoundToInt(Random.Range(0, universeSize.y-1));
+        float zPos = Mathf.RoundToInt(Random.Range(0, universeSize.z-1));
+
+        Vector3 voxelPosition = new Vector3(xPos, yPos, zPos);
+
+        if (createdVoxelPositions.Contains(voxelPosition))
+        {
+            Debug.Log($"Voxel not spawned at {voxelPosition}. Voxel already exists at that position.");
+            return;
+        }
+
+        createdVoxelPositions.Add(voxelPosition);
+
+        Debug.Log($"Checking neighbors for point X: {xPos}, Y: {yPos}, Z: {zPos}");
+
+        foreach (Vector3 offset in neighborOffsets)
+        {
+            Vector3 neighborPos = new Vector3(xPos, yPos, zPos) + offset;
+
+            if(neighborPos.x >= -1 && neighborPos.x < universeSize.x+1 &&
+                neighborPos.y >= -1 && neighborPos.y < universeSize.y+1 &&
+                neighborPos.z >= -1 && neighborPos.z < universeSize.z+1)
+            {
+                Voxel neighborVoxel = GetVoxelAtPosition(neighborPos);
+                if(neighborVoxel != null)
+                {
+                    Debug.Log($"Neighbor at X: {neighborPos.x}, Y: {neighborPos.y}, Z: {neighborPos.z} is {voxelMaterials[neighborVoxel.Type].name}");
+                }
+            }
+        }
+
+        int typeRandom = CalculateWeightedRandomType(xPos, yPos, zPos);
         int amountRandom = Random.Range(1, 10);
-        
-        float xPos = Mathf.RoundToInt(Random.Range(-universeSize.x, universeSize.x));
-        float yPos = Mathf.RoundToInt(Random.Range(-universeSize.y, universeSize.y));
-        float zPos = Mathf.RoundToInt(Random.Range(-universeSize.z, universeSize.z));
+        if (typeRandom >= 0 &&  typeRandom < voxelMaterials.Length)
+        {
+            Debug.Log($"Random possibility: {noSpawnRandom}. Creating a {voxelMaterials[typeRandom].name} voxel at X: {xPos}, Y: {yPos}, Z: {zPos}");
 
-        Debug.Log($"Coordinates: X: {xPos}, Y: {yPos}, Z: {zPos}");
+            GameObject voxelObject = Instantiate(voxelPrefab, new Vector3(xPos, yPos, zPos), Quaternion.identity);
 
+            Voxel voxelComponent = voxelObject.GetComponent<Voxel>();
 
-        GameObject voxelObject = Instantiate(voxelPrefab, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            voxelObject.GetComponent<Renderer>().material = voxelMaterials[typeRandom];
 
+            voxelComponent.Initialize(typeRandom, amountRandom, baseSpawnPossibilities[typeRandom], GetSpawnModifiers(typeRandom));
 
-        Voxel voxelComponent = voxelObject.GetComponent<Voxel>();
+            LogVoxelCounts();
+        }
+        else
+        {
+            Debug.LogError($"Invalid typeRandom value: {typeRandom}. It's outside the bounds of voxelMaterials array.");
+        }
+    }
 
+    Voxel GetVoxelAtPosition(Vector3 position)
+    {
+        if (createdVoxelPositions.Contains(position))
+        {
+            return new Voxel();
+        }
+        return null;
+    }
 
-        voxelComponent.Initialize(typeRandom, amountRandom);
+    float[] GetSpawnModifiers(int voxelType)
+    {
+        switch(voxelType)
+        {
+            case 0: return goldSpawnModifiers;
+            case 1: return silverSpawnModifiers;
+            case 2: return bronzeSpawnModifiers;
+            case 3: return platinumSpawnModifiers;
+            default: return new float[0];
+        }
+    }
 
-        LogVoxelCounts();
+    int CalculateWeightedRandomType(float x, float y, float z)
+    {
+        float bronzeWeight = 0.4f;
+        float silverWeight = 0.3f;
+        float goldWeight = 0.2f;
+        float platinumWeight = 0.1f;
+
+        float totalWeight = bronzeWeight + silverWeight + goldWeight + platinumWeight;
+
+        // Calculate the sum of weights of neighboring voxels
+        foreach (Vector3 offset in neighborOffsets)
+        {
+            Vector3 neighborPos = new Vector3(x, y, z) + offset;
+
+            if (neighborPos.x >= 0 && neighborPos.x < universeSize.x &&
+                neighborPos.y >= 0 && neighborPos.y < universeSize.y &&
+                neighborPos.z >= 0 && neighborPos.z < universeSize.z)
+            {
+                Voxel neighborVoxel = GetVoxelAtPosition(neighborPos);
+                if (neighborVoxel != null)
+                {
+                    switch (neighborVoxel.Type)
+                    {
+                        case 0: // Bronze
+                            totalWeight += 0f;
+                            break;
+                        case 1: // Silver
+                            totalWeight += 0.09f;
+                            break;
+                        case 2: // Gold
+                            totalWeight += 0.06f;
+                            break;
+                        case 3: // Platinum
+                            totalWeight += 0f;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        float randomValue = Random.Range(0f, totalWeight);
+
+        if (randomValue <= bronzeWeight)
+            return 0; // Bronze voxel type
+        else if (randomValue <= bronzeWeight + silverWeight)
+            return 1; // Silver voxel type
+        else if (randomValue <= bronzeWeight + silverWeight + goldWeight)
+            return 2; // Gold voxel type
+        else
+            return 3; // Platinum voxel type
+    }
+
+    void GeneratePlane()
+    {
+        for(int x=0;x < universeSize.x;x++)
+        {
+            for(int z=0;z < universeSize.z; z++)
+            {
+                GameObject voxelObject = Instantiate(voxelPrefab, new Vector3(x, -1, z), Quaternion.identity);
+            }
+        }
     }
 
     void LogVoxelCounts()
--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -5,26 +5,31 @@ public class Voxel : MonoBehaviour
     public int Type { get; private set; }
     public int Amount { get; private set; }
 
+    public float BaseSpawnPossibility { get; private set; }
+    public float[] NeighborSpawnModifiers { get; private set; }
+
     public static int goldVoxelCount = 0;
     public static int silverVoxelCount = 0;
     public static int bronzeVoxelCount = 0;
     public static int platinumVoxelCount = 0;
 
-    public void Initialize(int voxelType, int voxelAmount)
+    public void Initialize(int voxelType, int voxelAmount, float baseSpawnPossibility, float[] neighborSpawnModifiers)
     {
         Type = voxelType;
         Amount = voxelAmount;
+        BaseSpawnPossibility = baseSpawnPossibility;
+        NeighborSpawnModifiers = neighborSpawnModifiers;
         
         switch(Type)
         {
             case 0:
-                goldVoxelCount++;
+                bronzeVoxelCount++;
                 break;
             case 1:
                 silverVoxelCount++; 
                 break;
             case 2:
-                bronzeVoxelCount++; 
+                goldVoxelCount++; 
                 break;
             case 3:
                 platinumVoxelCount++; 
